ddd34288d5894831923d4ebead708bf945aacdcd
MatchesMalone7 <155798983+MatchesMalone7@users.noreply.github.com>
2026-04-10 10:43:34 -0700

    Worked on
    Basic structure of some of the programs, like variables (names, timelimit, timeused, and countdown) and methods.
    Not fully testable yet

---
+++ src/Filter.java
@@ -0,0 +1,18 @@
+public class Filter {
+    String name;
+    int timeLimit;
+    int timeUsed;
+    int countdown;
+
+    public void setTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    void startCountdown()
+
+
+}
