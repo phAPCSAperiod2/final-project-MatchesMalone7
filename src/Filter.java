b74973d9ef1fc4100affa776af663aadcd48f0e1
MatchesMalone7 <155798983+MatchesMalone7@users.noreply.github.com>
2026-04-13 10:57:23 -0700

    What I worked on: adjusting filter.java
    Working on the Filter overall, got constructors close to done, and gave the whole filter.java, so far, java doc comments using Microsoft Copilot. Deciding whether or not to adjust my sprint plan to induce making some of the TimeManger during this time. The actual rest of the filter.java is fine. I also created TimeManger, which is empty for the time being.

--- src/Filter.java
+++ src/Filter.java
@@ -1,18 +1,62 @@
+/**
+ * The Filter class represents one air filter the user wants to track.
+ * It stores the filter's name, its time limit, and how many hours
+ * have already been used. This class does not handle countdown logic.
+ * All java doc comments will be attributed to Mircosoft Copilot
+ */
 public class Filter {
-    String name;
-    int timeLimit;
-    int timeUsed;
-    int countdown;
 
-    public void setTimeLimit(int timeLimit) {
+    /** The name or label of the filter. */
+    private String nameOfFilter;
+
+    /** The total number of hours the filter should last. */
+    private int timeLimit;
+
+    /** The number of hours the filter has been used so far. */
+    private int hoursUsed;
+
+    /**
+     * Constructs a new Filter with a name and a time limit.
+     * hoursUsed starts at 0.
+     *
+     * @param nameOfFilter the name of the filter
+     * @param timeLimit    the number of hours the filter should last
+     */
+    public Filter(String nameOfFilter, int timeLimit) {
+        this.nameOfFilter = nameOfFilter;
         this.timeLimit = timeLimit;
+        this.hoursUsed = 0;
     }
 
-    public static void main(String[] args) {
-
+    /** @return the filter's name */
+    public String getName() {
+        return nameOfFilter;
     }
 
-    void startCountdown()
+    /** Sets a new name for the filter. */
+    public void setName(String newName) {
+        this.nameOfFilter = newName;
+    }
 
+    /** @return the time limit of the filter */
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    /**
+     * Sets a new time limit for the filter.
+     * 
+     * @param newLimit the new number of hours the filter should last
+     */
+    public void setTimeLimit(int newLimit) {
+        this.timeLimit = newLimit;
+    }
+
+    /** @return how many hours the filter has been used */
+    public int getHoursUsed() {
+        return hoursUsed;
+    }
+
+    // under contruction, for the constructors.
 
 }
